59acbf10a4134856d923feceef04b3f52666ae6d
Kyle Bulloch <kylekbulloch@gmail.com>
2015-03-06 09:37:02 -0800

    Finish app.php skeleton

--- address_book/app/app.php
+++ address_book/app/app.php
@@ -3,9 +3,29 @@
     require_once __DIR__."/../src/Contact.php";
 
     //session_start
+    session_start();
+    if(empty($_SESSION['contact_list'])) {
+        $_SESSION['contact_list'] = array();
+    }
+
     //new silex object
-    //$app->get("/", function use ($app) {
-    //});
+    $app = new Silex\Application();
 
+    $app->register(new Silex\Provider\TwigServiceProvider(), array(
+        'twig.path' => __DIR__.'/../views'
+    ));
 
+    $app->get("/", function use ($app) {
+        return $app['twig']->render('address_book_template.twig', array('contacts' => Contact::getAll()));
+    });
+
+    $app->post("/create_contact", function() use ($app) {
+        //return twig render
+    });
+
+    $app->post("/search_contacts", function() use ($app) {
+        //return twig render
+    });
+
+    return $app;
 ?>
