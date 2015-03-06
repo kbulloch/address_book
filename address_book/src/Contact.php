674dc168cf67796433429e1bdbde71d4731480b3
Kyle Bulloch <kylekbulloch@gmail.com>
2015-03-06 09:46:27 -0800

    Add setters, getters, static methods to Contact class

--- address_book/src/Contact.php
+++ address_book/src/Contact.php
@@ -15,9 +15,64 @@
             $this->phone_number = $new_phone_number;
             $this->address = $new_address;
         }
+
         //setters
+        function setFirstName($first_name_input)
+        {
+            $this->first_name = $first_name_input;
+        }
+
+        function setLastName($last_name_input)
+        {
+            $this->last_name = $last_name_input;
+        }
+
+        function setPhoneNumer($phone_number_input)
+        {
+            $this->phone_number = $phone_number_input;
+        }
+
+        function setAddress($address_input)
+        {
+            $this->address = $address_input;
+        }
+
         //getters
+        function getFirstName()
+        {
+            return $this->first_name;
+        }
+
+        function getLastName()
+        {
+            return $this->last_name;
+        }
+
+        function getPhoneNumber()
+        {
+            return $this->phone_number;
+        }
+
+        function getAddress()
+        {
+            return $this->address;
+        }
+
         //static methods
             //save, getAll, deleteAll
+        static function save()
+        {
+            array_push($_SESSION['contact_list'], $this);
+        }
+
+        static function getAll()
+        {
+            return $_SESSION['contact_list'];
+        }
+
+        static function deleteAll()
+        {
+            $_SESSION['contact_list'] = array();
+        }
     }
 ?>
