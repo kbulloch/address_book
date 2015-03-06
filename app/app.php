881f8a6732eb6b0a5fbec1144b359b8d11806718
Kyle <student@epicodus.com>
2015-03-06 13:59:20 -0800

    Add comments to app.php

--- app/app.php
+++ app/app.php
@@ -14,41 +14,44 @@
     ));
 
     $app->get("/", function() use ($app) {
+        //render the home page
         return $app['twig']->render('address_book_template.php', array('contacts' => Contact::getAll()));
     });
 
     $app->post("/create_contact", function() use ($app) {
 
+        //create a new contact based on user input
         $contact = new Contact($_POST['first_name'], $_POST['last_name'], $_POST['phone_number'], $_POST['address']);
         $contact->save();
 
+        //render the new contact page
         return $app['twig']->render('create_contact.php', array('newcontact' => $contact));
 
     });
 
     $app->post("/search_contacts", function() use ($app) {
 
-        //get user input
+        //get user search input
         $last_name_query = strtoupper($_POST['last_name_query']);
         //also add first name searching
 
         $matching_contacts = array();
 
-        //foreach loop thru contact list
+        //send matching contacts to new array
         foreach ($_SESSION['list_of_contacts'] as $a_contact) {
             if (strtoupper($a_contact->getLastName()) == $last_name_query) {
                 array_push($matching_contacts, $a_contact);
             }
         }
 
-        //output to new array with matching contacts
-        //send matching_contacts array to search_contacts.php
-
+        //render the search results page
         return $app['twig']->render('search_contacts.php', array('matching_contacts' => $matching_contacts));
+
     });
 
     $app->post("/delete_all", function() use ($app) {
 
+        //delete all contacts
         Contact::deleteAll();
         return $app['twig']->render('delete_all.php');
 
