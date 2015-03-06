dbea5edf6fa497fb57659fcb4fae4cde1254ea6c
Kyle Bulloch <kylekbulloch@gmail.com>
2015-03-06 12:07:52 -0800

    Sketch out search function in app.php only

--- app/app.php
+++ app/app.php
@@ -2,13 +2,11 @@
     require_once __DIR__."/../vendor/autoload.php";
     require_once __DIR__."/../src/Contact.php";
 
-    //session_start
     session_start();
     if(empty($_SESSION['list_of_contacts'])) {
         $_SESSION['list_of_contacts'] = array();
     }
 
-    //new silex object
     $app = new Silex\Application();
 
     $app->register(new Silex\Provider\TwigServiceProvider(), array(
@@ -16,9 +14,7 @@
     ));
 
     $app->get("/", function() use ($app) {
-
         return $app['twig']->render('address_book_template.php', array('contacts' => Contact::getAll()));
-
     });
 
     $app->post("/create_contact", function() use ($app) {
@@ -30,9 +26,26 @@
 
     });
 
-    // $app->post("/search_contacts", function() use ($app) {
-    //     //return twig render
-    // });
+    $app->post("/search_contacts", function() use ($app) {
+
+        //get user input
+        $last_name_query = strtoupper($_POST['last_name_query']);
+        //also add first name searching
+
+        $matching_contacts = array();
+
+        //foreach loop thru contact list
+        foreach ($_SESSION['list_of_contacts'] as $a_contact) {
+            if (strtoupper($a_contact->getLastName()) == $last_name_query) {
+                array_push($matching_contacts, $a_contact);
+            }
+        }
+
+        //output to new array with matching contacts
+        //send matching_contacts array to search_contacts.php
+
+        return $app['twig']->render('search_contacts.php', array('matching_contacts' => $matching_contacts))
+    });
 
     $app->post("/delete_all", function() use ($app) {
 
